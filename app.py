69d0d28192cfd28fd07bce1dbba180f37b36a5cf
Aleksey Kushnarev <scanavik2005@gmail.com>
2022-03-31 22:49:18 +0500

    create base.html and index.html

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
 def main():
-    db_session.global_init("db/blogs.db")
     app.run()
 
 
