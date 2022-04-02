86abebb9828e128c68f27dcc4652bea648bd1b3c
Aleksey Kushnarev <scanavik2005@gmail.com>
2022-04-02 20:57:52 +0500

    add choice theme button

---
+++ static/js/change_theme.js
@@ -0,0 +1,10 @@
+function change_theme() {
+    var checkbox = document.getElementById("color_theme");
+    if (checkbox.checked) {
+        $('#theme_icon').attr('src', '/static/img/icons/dark_theme.png');
+    }
+    else {
+        $('#theme_icon').attr('src', '/static/img/icons/light_theme.png');
+    }
+
+}
