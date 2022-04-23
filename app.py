6c8e414d26319977c38ad42b79f886151a4fee18
popov <artu.200505@gmail.com>
2022-04-23 13:21:21 +0500

    add login and register

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template, redirect
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+import datetime
+
+from flask import Flask, render_template, redirect, request, abort
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+from utils.db_api.forms.user import RegisterForm, LoginForm
+from utils.db_api.data.users import User
+from utils.db_api.data import db_session
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'universities_wiki_key'
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Логин', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
 
 
 @app.route('/')
@@ -37,17 +36,34 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
-# TODO
-@app.route('/register')
-def register():
-    form = LoginForm()
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
     if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('login.html', title='Регистрация', form=form)
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 def main():
-    app.run(port=8080, host='127.0.0.1')
+    # TODO: create database
+    db_file = ""
+    db_session.global_init(db_file)
+    app.run(host="127.0.0.1", port=8080)
 
 
 if __name__ == '__main__':
