3d3e8404ca0944209c4f6d6bc71f160bf3a0aa82
Aleksey Kushnarev <scanavik2005@gmail.com>
2022-04-23 13:20:07 +0500

    create profile.html

--- app.py
+++ app.py
@@ -4,13 +4,24 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
 
-@app.route('/universities')
-def universities():
-    return render_template('list_universities.html')
+@app.route('/universities/<city>')
+def universities(city):
+    params = {
+        'city': city
+    }
+    return render_template('list_universities.html', **params)
+
+
+@app.route('/profile/<int:user_id>')
+def profile(user_id):
+    params = {}
+    params['user_id'] = user_id
+    return render_template('profile.html', **params)
 
 
 def main():
