d334db80e58fe90b7134d4a12f1a8eb6346f8852
Aleksey Kushnarev <scanavik2005@gmail.com>
2022-04-25 22:54:15 +0500

    check

--- utils/db_api/config.py
+++ utils/db_api/config.py
@@ -7,8 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'sqlite:///' + os.path.join(basedir, 'db/database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../../db/database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
