df726032e6a38bfe18c5edd29c238bbce5e15bf6
annaalikina <alikinaan8@yandex.ru>
2022-04-30 18:42:25 +0500

    add password hashing

--- static/js/change_theme.js
+++ static/js/change_theme.js
@@ -1,4 +1,4 @@
- /*function change_theme() {
+function change_theme() {
     var checkbox = document.getElementById("color_theme");
     if (checkbox.checked) {
         $('#theme_icon').attr('src', '/static/img/icons/dark_theme.png');
@@ -9,25 +9,4 @@
     }
 
 }
-*/
-var btn = document.getElementById("theme-button");
-var link = document.getElementById("theme-link");
 
-btn.addEventListener("click", function () { ChangeTheme(); });
-
-function ChangeTheme()
-    var theme = "";
-    let lightTheme = "styles/light.css";
-    let darkTheme = "styles/dark.css";
-    var checkbox = document.getElementById("color_theme");
-    if (checkbox.checked) {
-        $('#theme_icon').attr('src', '/static/img/icons/dark_theme.png');
-        currTheme = darkTheme;
-   	    theme = "dark_theme"
-    }
-    else {
-        $('#theme_icon').attr('src', '/static/img/icons/light_theme.png');
-        theme = "color_theme"
-
-    Save(theme);
-}
