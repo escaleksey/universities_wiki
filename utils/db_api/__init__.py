925a107ddac75559b450791557ae537f931eae78
annaalikina <alikinaan8@yandex.ru>
2022-05-01 19:19:11 +0500

    make todo

--- utils/db_api/__init__.py
+++ utils/db_api/__init__.py
@@ -1,3 +1,3 @@
 from .data import db_session
 
-from utils.db_api.models import User, University, Faculty
+from utils.db_api.models import User, University, Faculty, association_table
--- utils/db_api/main.py
+++
@@ -1,4 +0,0 @@
-import repository
-
-if __name__ == "__main__":
-    repository.create_user()
